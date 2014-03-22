0306d0248a3094dbb65b4778261798d9490bada1
Anthony Ross <anthony.s.ross@gmail.com>
2014-03-22 15:46:36 -0400

    Added further controller help & table defs

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(params[:post].permit!)
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @post.update(params[:post].permit!)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_url
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:title)
+  end
+
+end
