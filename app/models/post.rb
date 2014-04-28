3e1933ba2ebc3f2346fcc9b66c6b1e5481db5e46
Anthony Ross <anthony.s.ross@gmail.com>
2014-04-28 10:32:40 -0400

    Refactored tests to use shoulda-matchers and also cleaned up some existing test files generated by rspec.  There is still more testing to be done since building this at the hackathon but core functionality seems to still work.  I changed the uniqueness scope of job_posting_url to be by user.  Also removed the ability to edit the local_url.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :status, presence: true
-  validates :job_posting_url, uniqueness: true
+  validates :job_posting_url, uniqueness: { scope: :user_id }
   belongs_to :user
+  validates :user, presence: true
   belongs_to :job_board
 
   def http_fetch(url)
--- spec/controllers/webcontent_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe WebcontentController do
-
-end
